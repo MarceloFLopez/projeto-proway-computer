bbea38e3de2697c9263c93a21798141b8468a411
unknown <marceloferreiralopez@gmail.com>
2024-03-14 11:26:19 -0300

    angular material instalado, teste evento de click ativado

--- src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
+++ src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NotificacaoService } from 'src/app/notificacao.service';
 import { IProduto } from 'src/app/produtods';
 import { ProdutosService } from 'src/app/produtos.service';
 
@@ -14,13 +15,19 @@ export class DetalhesProdutosComponent implements OnInit {
   
   constructor(
     private produtoService: ProdutosService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService
   ) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const produtoId = Number(routeParams.get("id"));
     this.produto = this.produtoService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O Produto foi adicionado!");
+    console.log("botao ativado!")
   }
 
 }

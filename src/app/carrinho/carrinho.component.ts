83c8ee08d4e3fede0e690d3092522af47ce49a0c
unknown <marceloferreiralopez@gmail.com>
2024-03-16 11:33:30 -0300

    funcionalidads do carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtods';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
+  itenCarrinho: IProdutoCarrinho[] = [];
+  total: number = 0;
 
-  constructor() { }
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
+    this.itenCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal()
   }
 
+  removeProdutoCarrinho(produtoId: number){
+    this.itenCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoDoCarrinho(produtoId);
+    location.reload();
+  }
+
+  calculaTotal(){
+    return this.total = this.itenCarrinho.reduce((prev, curr) => 
+    (prev + curr.preco * curr.quantidade), 0);
+  }
+
+  comprar(){
+    alert("Parabéns, você finalizou sua comprar");
+    this.carrinhoService.limparCarrinho();
+    this.route.navigate(["produtos"]);
+  }
 }
